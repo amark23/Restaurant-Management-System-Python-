c04419ef90dbda1b877c5c7f0cf0cc6aaf7974b9
Amar Kumar <amar.kumar919@gmail.com>
2022-10-03 00:50:35 +0530

    Update restaurant.py

--- restaurant.py
+++ restaurant.py
@@ -18,7 +18,7 @@ f2.pack(side=RIGHT)
 #------------------TIME--------------
 localtime=time.asctime(time.localtime(time.time()))
 #-----------------INFO TOP------------
-lblinfo = Label(Tops, font=( 'aria' ,30, 'bold' ),text="Restaurant Management System",fg="steel blue",bd=10,anchor='w')
+lblinfo = Label(Tops, font=( 'aria' ,30, 'bold' ),text="Restaurant Management System By Amar Kumar",fg="steel blue",bd=10,anchor='w')
 lblinfo.grid(row=0,column=0)
 lblinfo = Label(Tops, font=( 'aria' ,20, ),text=localtime,fg="steel blue",anchor=W)
 lblinfo.grid(row=1,column=0)
